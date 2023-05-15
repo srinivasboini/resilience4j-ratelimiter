2aa1a91101491ff3b197ef9b5d14bb064ac64d0f
Srini <srinivasboini7@gmail.com>
2023-05-15 22:08:04 +0800

    Initial commit

---
+++ src/test/java/com/srini/ratelimit/Resilience4jRatelimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srini.ratelimit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jRatelimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
