91d308aba3fa4eb9a1960f33c58d80725d2f1b96
Srini <srinivasboini7@gmail.com>
2023-05-15 22:26:20 +0800

    final working code

---
+++ src/main/java/com/srini/ratelimit/controller/AppController.java
@@ -0,0 +1,29 @@
+package com.srini.ratelimit.controller;
+
+import com.srini.ratelimit.service.AppService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.IntStream;
+
+@RestController
+@RequestMapping("/app")
+@Slf4j
+@RequiredArgsConstructor
+public class AppController {
+
+    private final AppService appService ;
+
+    @GetMapping("/rate")
+    public String get(){
+        //fake calls to reproduce concurrent calls to exceed the rate limit
+        IntStream.rangeClosed(1,11)
+                .parallel()
+                .forEach(e -> log.info("{}", appService.getDefault()));
+
+        return appService.getDefault() ;
+    }
+}
---
+++ src/main/java/com/srini/ratelimit/service/AppService.java
@@ -0,0 +1,27 @@
+package com.srini.ratelimit.service;
+
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Service
+@Slf4j
+public class AppService {
+
+    private AtomicInteger counter = new AtomicInteger() ;
+
+    @RateLimiter(name = "serviceA", fallbackMethod = "fallbackA")
+    public String getDefault(){
+        log.info("{} -> counter {} ", Thread.currentThread().getName(), counter.incrementAndGet());
+        return "Get from default" ;
+    }
+
+
+    private String fallbackA(Throwable throwable){
+        log.error("{}", throwable);
+        counter.set(0);
+        return "fallbackA" ;
+    }
+}
